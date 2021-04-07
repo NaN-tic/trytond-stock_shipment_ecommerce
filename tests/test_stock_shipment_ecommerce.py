dde52998963f8d8bbecc72391960edb9dacbbe69
Raimon Esteve <raimonesteve@gmail.com>
2021-04-07 11:28:05 +0200

    Add test to adapt issue10053

--- tests/test_stock_shipment_ecommerce.py
+++ tests/test_stock_shipment_ecommerce.py
@@ -5,14 +5,25 @@ import unittest
 import doctest
 
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.tests.test_tryton import doctest_teardown, doctest_checker
+from trytond.pool import Pool
 
 
 class StockShipmentEcommerceTestCase(ModuleTestCase):
     'Test Stock Shipment Ecommerce module'
     module = 'stock_shipment_ecommerce'
 
+    @with_transaction()
+    def test_party_identifiers(self):
+        'Party Identifiers'
+        pool = Pool()
+        Configuration = pool.get('party.configuration')
+        PartyIdentifier = pool.get('party.identifier')
+
+        _TYPE = ('shop', 'Shop')
+        self.assertTrue(_TYPE in Configuration.identifier_types.selection)
+        self.assertTrue(_TYPE in PartyIdentifier.get_types())
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
